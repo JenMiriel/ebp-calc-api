16bd227fc3e654a400130a46d8edf3a12a6e0c8b
Jen Wood <jen.miriel@gmail.com>
2021-01-09 21:22:12 -0700

    fixed seed data migration

--- EmplBeniPkgCalc/Migrations/20210110040830_AddSeedData.cs
+++ EmplBeniPkgCalc/Migrations/20210110040830_AddSeedData.cs
@@ -26,22 +26,22 @@ namespace EmployeeBenefitPackageCalc.Migrations
                 values: new object[] { 1, 500, 10, "A", 1000, "Hyrule Health Network" });
 
             migrationBuilder.InsertData(
-                table: "Dependants",
+                table: "Dependant",
                 columns: new[] { "Id", "IsSpouse", "EmployeeId", "FirstName",  "LastName", "BirthDate"},
                 values: new object[,]
                 {
                     { 1, true, 1, "Morticia", "Addams", new DateTime(1964, 12, 18, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 1, "Wednesday", "Addams", new DateTime(2007, 5, 3, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 1, "Pugsley", "Addams", new DateTime(2009, 7, 29, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 1, "Grandmama", "Addams", new DateTime(1942, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, true, 2, "Helen", "Parr", new DateTime(1980, 2, 25, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 2, "Violet", "Parr", new DateTime(2005, 9, 9, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 2, "Dash", "Parr", new DateTime(2011, 4, 30, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 2, "Jack-Jack", "Parr", new DateTime(2019, 11, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 5, "John", "Connor", new DateTime(2006, 5, 14, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, true, 6, "Iduna", "Runeardsson", new DateTime(1977, 3, 31, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 6, "Elsa", "Runeardsson", new DateTime(2010, 12, 12, 0, 0, 0, 0, DateTimeKind.Unspecified) },
-                    { 1, false, 6, "Anna", "Runeardsson", new DateTime(2013, 6, 8, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, false, 1, "Wednesday", "Addams", new DateTime(2007, 5, 3, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, false, 1, "Pugsley", "Addams", new DateTime(2009, 7, 29, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, false, 1, "Grandmama", "Addams", new DateTime(1942, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, true, 2, "Helen", "Parr", new DateTime(1980, 2, 25, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 6, false, 2, "Violet", "Parr", new DateTime(2005, 9, 9, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 7, false, 2, "Dash", "Parr", new DateTime(2011, 4, 30, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 8, false, 2, "Jack-Jack", "Parr", new DateTime(2019, 11, 4, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 9, false, 5, "John", "Connor", new DateTime(2006, 5, 14, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 10, true, 6, "Iduna", "Runeardsson", new DateTime(1977, 3, 31, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 11, false, 6, "Elsa", "Runeardsson", new DateTime(2010, 12, 12, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 12, false, 6, "Anna", "Runeardsson", new DateTime(2013, 6, 8, 0, 0, 0, 0, DateTimeKind.Unspecified) },
                 });
 
         }
